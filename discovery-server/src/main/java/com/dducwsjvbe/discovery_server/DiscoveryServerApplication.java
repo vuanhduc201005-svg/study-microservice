cf0a09b19c8de057a25d08b067a04cf37f80be37
vuanhduc201005-svg <vuanhduc201005@gmail.com>
2026-06-24 16:20:29 +0700

    main

---
+++ discovery-server/src/main/java/com/dducwsjvbe/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.dducwsjvbe.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
